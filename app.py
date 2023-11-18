d603c2fb9b0a1f56f125ec03d7905a3b69941bca
Liam Cooney <liamcooneyh@gmail.com>
2023-11-17 21:46:13 -0500

    gets rid of redunant variable calls, simplifies process flow by removing other pages for time being. Allows for focus on auth and landing pages specifically before we can move on to the next step

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
-from flask import Flask, session
+# app.py
+from flask import Flask
 from auth import auth_bp
-from landing_page import landing_bp
-from user_profile import user_profile_bp
-from current_track import current_track_bp
+
+# Import other blueprints as needed
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = '7d266374106b85802220b6d77d03214f05f6581d45ee9d7c25c4984b2d0f972e'
+app.secret_key = os.getenv('FLASK_SECRET_KEY')  
 
-# Retrieve environment variables 
+# Retrieve environment variables
 CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 
 app.register_blueprint(auth_bp)
-app.register_blueprint(landing_bp)
-app.register_blueprint(user_profile_bp)
-app.register_blueprint(current_track_bp)
-
+# Register other blueprints as needed
--- auth.py
+++ auth.py
@@ -1,6 +1,14 @@
 # auth.py
 from flask import Blueprint, redirect, render_template, session
 from spotipy.oauth2 import SpotifyOAuth
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 
 auth_bp = Blueprint('auth', __name__)
 sp_oauth = SpotifyOAuth(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, scope='user-library-read')
@@ -9,6 +17,12 @@ sp_oauth = SpotifyOAuth(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, scope='user-libr
 def index():
     if not session.get('token_info'):
         auth_url = sp_oauth.get_authorize_url()
+        print('test')
         return redirect(auth_url)
-    
+
+    # Handle authenticated user's actions here
     return redirect('/landing')
+
+@auth_bp.route('/landing')
+def landing():
+    return render_template('landing.html')
