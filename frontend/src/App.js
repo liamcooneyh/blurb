b8cd6c6fec8c3032ef58f54fe512f1a7641d8799
Liam Cooney <liamcooneyh@gmail.com>
2023-12-04 23:21:53 -0500

    cleans up a lot of old files that were no longer being used. cleans up code and removes unnecessary modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,50 @@
 import React, { useState, useEffect } from 'react';
 
+const API_BASE_URL = 'http://localhost:5000';
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  // eslint-disable-next-line
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true); // Add isLoading state
+  const [recentlyPlayed, setRecentlyPlayed] = useState([])
 
   useEffect(() => {
-    console.log('Effect: Fetching token and user info...');
+    const api_url = (`${API_BASE_URL}/v1/api/get-token`);
 
-    // Make a GET request to your Flask API to fetch the token and user info
-    fetch('/api/user-info')
-      .then((response) => {
-        console.log('Response received');
-        return response.json();
-      })
-      .then((data) => {
-        console.log('Data received:', data);
+    fetch(api_url)
+      .then(response => response.json())
+      .then(data => {
+        const accessToken = data.access_token;
+        setToken(accessToken);
+        console.log('Access Token: ', accessToken);
 
-        if (data && data.access_token) {
-          console.log('Token received from Flask:', data.access_token);
-          // Token received from Flask, store it in local state
-          setToken(data.access_token);
-        }
-
-        if (data && data.user_info) {
-          console.log('User info received:', data.user_info);
-          // User info received from Flask, store it in local state
-          setUser(data.user_info);
-        }
-
-        // Set isAuthenticated to true if either token or user_info is available
-        if (data && (data.access_token || data.user_info)) {
-          setIsAuthenticated(true);
-        }
-
-        // Set isLoading to false when authentication data is received
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setIsLoading(false); // Handle loading state even on error
+        setIsAuthenticated(true);
+        return accessToken;
       });
-  }, []);
+    }, []); 
 
-  console.log('Render: isAuthenticated =', isAuthenticated);
-
-  if (isLoading) {
-    // Render loading spinner or progress bar while data is being fetched
-    return <div>Loading...</div>;
-  }
+  
+    // useEffect(() => {
+    //   const api_url = (`${API_BASE_URL}/v1/api/recently-played`);
+      
+    //   fetch(api_url)
+    //     .then(response => response.json());
+    //     console.log(response);
+    // }, []);
+  
 
   if (!isAuthenticated) {
-    return (
-      <div>
-        <a href="http://localhost:5000">Log in</a>
-      </div>
-    );
-  }
+    return <div><a href="http://localhost:5000">Log in</a></div>;
+  };
 
-  // If the user is authenticated and user information is available, you can display it
   return (
     <div>
       {user && (
         <div>
-          <h3>User Information:</h3>
-          <p>Name: {user.display_name}</p>
-          <p>Email: {user.email}</p>
+          <h1>Authenticated</h1>
         </div>
       )}
-      {/* protected content goes here */}
     </div>
   );
 }
